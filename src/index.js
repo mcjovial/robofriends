f66639841f0d0868c0a0aed91754d5f795e829d9
mcjovial <emmanuelelias455@gmail.com>
2021-09-28 01:40:48 +0100

    redux async actions

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider, connect } from 'react-redux';
+import { Provider } from 'react-redux';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons'; 
-import { applyMiddleware, createStore } from 'redux';
-import { searchRobots } from './reducers';
+import { applyMiddleware, createStore, combineReducers } from 'redux';
+import { searchRobots, requestRobots } from './reducers';
 
-const store = createStore(searchRobots, applyMiddleware(logger));
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 
 ReactDOM.render(
   <Provider store={store}>
